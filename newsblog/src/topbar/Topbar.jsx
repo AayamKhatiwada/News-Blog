3fbd13e8123e93405cdf70defe3a572b586bf1ad
Aayam Khatiwada <aayamkhatiwada123@gmail.com>
2022-07-25 11:18:38 +0545

    update app.js

---
+++ newsblog/src/topbar/Topbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Topbar.css'
+
+export default function Topbar() {
+  return (
+    <div className='top'>
+        <div className="topleft">
+        <i class="fa-brands fa-facebook-square"></i>
+        <i class="fa-brands fa-twitter-square"></i>
+        <i class="fa-brands fa-instagram-square"></i>
+        </div>
+        <div className="topcenter">2</div>
+        <div className="topright">3</div>
+    </div>
+  )
+}
