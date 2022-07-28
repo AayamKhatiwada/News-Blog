5cf433ab5c8205b133351486d5505637327f1671
Aayam Khatiwada <aayamkhatiwada123@gmail.com>
2022-07-28 18:21:31 +0545

    Update App.js

--- newsblog/src/App.js
+++ newsblog/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Home from './pages/home/home';
 import Topbar from './topbar/Topbar';
 
 function App() {
   return (
     <div>
       <Topbar/>
+      <Home/>
       
     </div>
   );
