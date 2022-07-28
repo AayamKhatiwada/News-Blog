1e5452fe6a531dec1ca45ec8b46c0e314cca72f1
Aayam Khatiwada <aayamkhatiwada123@gmail.com>
2022-07-28 18:21:43 +0545

    Update Topbar

--- newsblog/src/topbar/Topbar.jsx
+++ newsblog/src/topbar/Topbar.jsx
@@ -4,13 +4,24 @@ import './Topbar.css'
 export default function Topbar() {
   return (
     <div className='top'>
-        <div className="topleft">
-        <i class="fa-brands fa-facebook-square"></i>
-        <i class="fa-brands fa-twitter-square"></i>
-        <i class="fa-brands fa-instagram-square"></i>
-        </div>
-        <div className="topcenter">2</div>
-        <div className="topright">3</div>
+      <div className="topLeft">
+        <i className="topIcon fa-brands fa-twitter-square"></i>
+        <i className="topIcon fa-brands fa-facebook-square"></i>
+        <i className="topIcon fa-brands fa-instagram-square"></i>
+      </div>
+      <div className="topCenter">
+        <ui className="topList">
+          <li className="topListItem">Home</li>
+          <li className="topListItem">About</li>
+          <li className="topListItem">Contact</li>
+          <li className="topListItem">Write</li>
+          <li className="topListItem">LogOut</li>
+        </ui>
+      </div>
+      <div className="topRight">
+        <img src="https://images.pexels.com/photos/8947692/pexels-photo-8947692.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" className="topImage" />
+        <i className="topSearchIcon fas fa-search"></i>
+      </div>
     </div>
   )
 }
