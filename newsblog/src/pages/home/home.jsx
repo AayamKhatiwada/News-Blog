4726f8ae48603fb82461cf40f1827f93652422c7
Aayam Khatiwada <aayamkhatiwada123@gmail.com>
2022-08-07 09:14:10 +0545

    Update home.jsx

--- newsblog/src/pages/home/home.jsx
+++ newsblog/src/pages/home/home.jsx
@@ -1,6 +1,6 @@
 import Header from "../../items/header/header"
 import Posts from "../../items/posts/posts"
-import Sidebar from "../../items/sidebar/sidebar"
+import Sidebar from "../../items/sidebar/Sidebar"
 import "./home.css"
 
 export default function Home() {
