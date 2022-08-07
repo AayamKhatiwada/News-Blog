4b8b4e90bf4cde1309e3c0fbce06dfef19f73584
Aayam Khatiwada <aayamkhatiwada123@gmail.com>
2022-08-07 09:14:12 +0545

    Update Login.jsx

--- newsblog/src/pages/login/Login.jsx
+++ newsblog/src/pages/login/Login.jsx
@@ -1,4 +1,5 @@
 import "./login.css"
+import {Link} from 'react-router-dom'
 
 export default function Login() {
     return (
@@ -11,7 +12,9 @@ export default function Login() {
                 <input type="Password" className="loginInput" placeholder="Enter your Password" />
                 <button className="loginButton">Login</button>
             </form>
-            <button className="loginRegisterButton">Register</button>
+            <button className="loginRegisterButton">
+            <Link to="/register" style={{ textDecoration: "none", color: "inherit" }}>Register</Link>
+            </button>
         </div>
     )
 }
