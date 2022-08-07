9e32c4b06a86408a97deff52db5bfe1588112fb3
Aayam Khatiwada <aayamkhatiwada123@gmail.com>
2022-08-07 09:14:04 +0545

    Update App.js

--- newsblog/src/App.js
+++ newsblog/src/App.js
@@ -1,13 +1,28 @@
 import './App.css';
 import Topbar from './items/topbar/Topbar';
+import Home from "./pages/home/home"
 import Register from './pages/register/Register';
+import Login from './pages/login/Login'
+import Write from './pages/write/Write'
+import Settings from './pages/settings/Settings'
+import Single from './pages/single/Single';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
+  const user = true;
   return (
-    <div>
+    <Router>
       <Topbar />
-      <Register/>
-    </div>
+      <Routes>
+        <Route exact path="/" element={<Home/>}/>
+        {user ? <Route exact path="/register" element={<Register/>}/> : <Route exact path="/register" element={<Home/>}/>}
+        {user ? <Route exact path="/login" element={<Login/>}/> : <Route exact path="/login" element={<Home/>}/>}
+        {user ? <Route exact path="/write" element={<Register/>}/> : <Route exact path="/write" element={<Write/>}/>}
+        {user ? <Route exact path="/settings" element={<Settings/>}/> : <Route exact path="/settings" element={<Register/>}/>}
+        <Route exact path="/post/:postId" element={<Single/>}/>    
+      </Routes>
+    </Router>
+
   );
 }
 
