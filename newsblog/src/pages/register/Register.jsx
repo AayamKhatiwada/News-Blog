c2f9a7b9c0be8e13fc620e791580fb9b60ca4d08
Aayam Khatiwada <aayamkhatiwada123@gmail.com>
2022-08-07 09:14:15 +0545

    Update Register.jsx

--- newsblog/src/pages/register/Register.jsx
+++ newsblog/src/pages/register/Register.jsx
@@ -1,4 +1,5 @@
 import "./register.css"
+import {Link} from 'react-router-dom'
 
 export default function Register() {
     return (
@@ -13,7 +14,9 @@ export default function Register() {
                 <input type="Password" className="registerInput" placeholder="Enter your Password" />
                 <button className="registerButton">Register</button>
             </form>
-            <button className="registerLoginButton">Login</button>
+            <button className="registerLoginButton">
+                <Link to="/login" style={{ textDecoration: "none", color: "inherit" }}>Login</Link>
+            </button>
         </div>
     )
 }
