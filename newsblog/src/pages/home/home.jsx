a0e4ca51d847dad36d5acf0206553f86d31667d3
Aayam Khatiwada <aayamkhatiwada123@gmail.com>
2022-08-08 11:41:58 +0545

    Hosting

--- newsblog/src/pages/home/home.jsx
+++ newsblog/src/pages/home/home.jsx
@@ -1,9 +1,22 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
 import Header from "../../items/header/header"
 import Posts from "../../items/posts/posts"
 import Sidebar from "../../items/sidebar/Sidebar"
 import "./home.css"
 
 export default function Home() {
+
+  const [posts, setPosts] = useState([]);
+
+  useEffect(()=>{
+    const fetchPosts = async ()=>{
+      const res = await axios.get("/posts")
+      console.log(res)
+    }
+    fetchPosts()
+  },[])
+
   return (
     <>
       <Header />
