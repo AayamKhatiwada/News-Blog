8f557e68d70f87cc4b577d36deac334745ec28c4
Aayam Khatiwada <aayamkhatiwada123@gmail.com>
2022-08-12 19:41:59 +0545

    Update index.js

--- api/index.js
+++ api/index.js
@@ -3,12 +3,17 @@ const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 
+const authroutes = require("./routes/auth");
+
 dotenv.config();
+app.use(express.json());
 
 // code to connect to mongodb
 mongoose.connect(process.env.MONGO_URL)
 .then(console.log("Conect"))
 .catch(console.log((err) => console.log(err)));
+
+app.use("/api/auth", authroutes)
 
 app.listen("5000", () => {
     console.log("Backend is running.")
