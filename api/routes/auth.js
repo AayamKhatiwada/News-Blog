ba9e358d29f87cd41b34206e978de0bc66271037
Aayam Khatiwada <aayamkhatiwada123@gmail.com>
2022-08-14 08:47:22 +0545

    Update auth.js

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -31,12 +31,14 @@ router.post("/register", async(req,res)=>{
 router.post("/login", async (req,res)=>{
     try{
         const user = await User.findOne({userName: req.body.userName})
-        !user && res.status(400).json("wrong username")
+        !user && res.status(400).json("wrong username");
 
         const validated = await bcrypt.compare(req.body.password, user.password);
-        !validated && res.status(400).json("wrong password")
+        !validated && res.status(400).json("wrong password");
 
-        res.status(200).json(user)
+        const {password, ...others} = user._doc;
+
+        res.status(200).json(others)
     }catch(err){
         res.status(500).json(err)
     }
