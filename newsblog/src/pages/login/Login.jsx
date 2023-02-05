163cfc9697030863294744455573a6fcfd44279a
Aayam <aayamkhatiwada123@gmail.com>
2023-02-05 21:07:15 +0545

    Using context and localstorage to store user data

--- newsblog/src/pages/login/Login.jsx
+++ newsblog/src/pages/login/Login.jsx
@@ -1,19 +1,46 @@
 import "./login.css"
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import { useContext, useRef } from "react";
+import { Context } from "../../context/Context";
+import axios from "axios";
 
 export default function Login() {
+
+    const userRef = useRef();
+    const passwordRef = useRef();
+    const { user, dispatch, isFetching } = useContext(Context);
+
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        dispatch({ type: "LOGIN_START" })
+
+        try {
+            const res = await axios.post("/auth/login", {
+                userName: userRef.current.value,
+                password: passwordRef.current.value,
+            })
+
+            dispatch({ type: "LOGIN_SUCCESS", payload: res.data })
+        } catch (err) {
+            dispatch({ type: "LOGIN_FAILURE" })
+        }
+    }
+
+    console.log(user)
+
     return (
         <div className="login">
             <span className="loginTitle">Login</span>
-            <form className="loginForm">
-                <label>Email</label>
-                <input type="text" className="loginInput" placeholder="Enter your email" />
+            <form className="loginForm" onSubmit={handleSubmit}>
+                <label>User name</label>
+                <input type="text" className="loginInput" placeholder="Enter your User name" ref={userRef} />
                 <label>Password</label>
-                <input type="Password" className="loginInput" placeholder="Enter your Password" />
-                <button className="loginButton">Login</button>
+                <input type="Password" className="loginInput" placeholder="Enter your Password" ref={passwordRef} />
+                <button className="loginButton" disabled={isFetching}>Login</button>
             </form>
             <button className="loginRegisterButton">
-            <Link to="/register" style={{ textDecoration: "none", color: "inherit" }}>Register</Link>
+                <Link to="/register" style={{ textDecoration: "none", color: "inherit" }}>Register</Link>
             </button>
         </div>
     )
--- newsblog/src/pages/register/Register.jsx
+++ newsblog/src/pages/register/Register.jsx
@@ -1,5 +1,5 @@
 import "./register.css"
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { useState } from "react"
 import axios from "axios";
 
@@ -8,16 +8,23 @@ export default function Register() {
     const [userName, setUserName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
 
-    const submitForm = async(e) => {
+    const submitForm = async (e) => {
         e.preventDefault();
+        setError(false)
 
-        const res = await axios.post("/auth/register", {
-            userName,
-            email,
-            password
-        });
-        console.log(res)
+        try {
+            const res = await axios.post("/auth/register", {
+                userName,
+                email,
+                password
+            });
+
+            res.data && window.location.replace("/login")
+        } catch (err) {
+            setError(true)
+        }
     }
 
     return (
@@ -25,16 +32,19 @@ export default function Register() {
             <span className="registerTitle">Register</span>
             <form className="registerForm" onSubmit={submitForm}>
                 <label>Username</label>
-                <input type="text" className="registerInput" placeholder="Enter your Username" value={userName} onChange={(e) => setUserName(e.target.value)}/>
+                <input type="text" className="registerInput" placeholder="Enter your Username" value={userName} onChange={(e) => setUserName(e.target.value)} />
                 <label>Email</label>
-                <input type="text" className="registerInput" placeholder="Enter your email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+                <input type="text" className="registerInput" placeholder="Enter your email" value={email} onChange={(e) => setEmail(e.target.value)} />
                 <label>Password</label>
-                <input type="Password" className="registerInput" placeholder="Enter your Password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+                <input type="Password" className="registerInput" placeholder="Enter your Password" value={password} onChange={(e) => setPassword(e.target.value)} />
                 <button className="registerButton">Register</button>
             </form>
             <button className="registerLoginButton">
                 <Link to="/login" style={{ textDecoration: "none", color: "inherit" }}>Login</Link>
             </button>
+            {
+                error && <span style={{ color: "red", marginTop: "10px" }}>Something went wrong</span>
+            }
         </div>
     )
 }
